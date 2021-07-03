533ec7932ec27300e5bb3630338182dfb96e5e96
kawara0329 <a.kawara0329@gmail.com>
2021-07-03 18:27:21 +0900

    アクティブストレージの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,10 @@ class ItemsController < ApplicationController
   def move_to_index
     redirect_to action: :index unless user_signed_in?
   end
+
+  private
+
+  def message_params
+    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
