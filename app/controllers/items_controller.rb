ceeed2948339f63aacbfcb398c724aa9d77da6cf
kawara0329 <a.kawara0329@gmail.com>
2021-07-06 19:05:29 +0900

    コントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:content, :image, :name, :detail, :category_id, :status_id, :shipping_charge_id, :prefecture_id, :delivery_day_id, :price,).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :detail, :category_id, :status_id, :shipping_charge_id, :prefecture_id, :delivery_day_id, :price).merge(user_id: current_user.id)
   end
 end
