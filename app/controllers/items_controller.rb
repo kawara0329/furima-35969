90e72e216c0107a36352a4e6d1e37f4de629e458
kawara0329 <a.kawara0329@gmail.com>
2021-07-08 21:40:29 +0900

    コントローラー追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :find_params, only: [:show, :edit, :update, :destroy]
-  before_action :move_to_index, only: [:edit, :update]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
   def index
     @item = Item.includes(:user).order("created_at DESC")
   end
